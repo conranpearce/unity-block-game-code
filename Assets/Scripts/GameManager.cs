d165f9337baec0b2cab70e643edf27fd514221d9
conranpearce <conran2.pearce@live.uwe.ac.uk>
2019-11-13 12:36:05 +0000

    Inital Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject completeLevel;
+
+    public void CompleteLevel()
+    {
+        completeLevel.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Restart();
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public PlayerMovement movement;
+
+    void Update()
+    {
+        if (player.position.z >= 0 && player.position.z < 150)
+        {
+            scoreText.text = player.position.z.ToString("0");
+        }
+        else if (player.position.z < 0)
+        {
+            scoreText.text = "0";
+        }
+        else if (player.position.z >= 150)
+        {
+            if (player.position.z >= 200)
+            {
+                FindObjectOfType<GameManager>().EndGame();
+
+            } else
+            {
+                scoreText.text = "150";
+                movement.controls = false;
+            }
+        }
+    }
+   
+}
